b925e656ae0fc8c8f5a4b9fbeeda96c2bd33dcf7
xHero <loergame@gmail.com>
2023-10-24 09:18:43 +0200

    Initial Commit

---
+++ Argus Main/Argus_MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Argus_MainApp.swift
+//  Argus Main
+//
+//  Created by Carmine Andreozzi on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Argus_MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
